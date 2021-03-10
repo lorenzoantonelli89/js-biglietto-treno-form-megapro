b45881c61c2fd2aac657305c5a27dd309dba0246
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-03-10 21:44:06 +0100

    esercizio finito

--- js/first.js
+++ js/first.js
@@ -1,1 +1,49 @@
-console.log('hello');
+
+var generatesBtn = document.getElementById('genera');
+var cancelBtn = document.getElementById('annulla');
+var price = 0.21;
+// click su genera
+
+generatesBtn.addEventListener('click', function(){
+  var name = document.getElementById('name').value;
+  var distance = parseInt(document.getElementById('distance').value);
+  var age = document.getElementById('age').value;
+  var totalPrice = price * distance;
+
+  if (name != '' && distance > 0 && age != '') {
+    var carriage = Math.floor(Math.random() * 20) + 1;
+    var code = Math.floor(Math.random() * 100000 - 90000) + 90000;
+    document.getElementById('namepass').innerHTML = name;
+    document.getElementById('offerta').innerHTML = age;
+    document.getElementById('carrozza').innerHTML = carriage;
+    document.getElementById('codiceCP').innerHTML = code;
+
+    if (age == 1) {
+      totalPrice = (totalPrice / 100 * 80).toFixed(2);
+      document.getElementById('offerta').innerHTML = 'Sconto 20%';
+      document.getElementById('costobigl').innerHTML = totalPrice;
+    }else if (age == 3) {
+      totalPrice = (totalPrice / 100 * 60).toFixed(2);
+      document.getElementById('offerta').innerHTML = 'Sconto 40%';
+      document.getElementById('costobigl').innerHTML = totalPrice;
+    }else {
+      document.getElementById('offerta').innerHTML = 'Prezzo Pieno';
+      document.getElementById('costobigl').innerHTML = totalPrice;
+    }
+
+
+  }else {
+    alert('Non hai inserito correttamente tutti i dati riprova!')
+  }
+
+});
+
+// click su annulla
+
+cancelBtn.addEventListener('click', function(){
+  document.getElementById('namepass').innerHTML = '';
+  document.getElementById('offerta').innerHTML = '';
+  document.getElementById('carrozza').innerHTML = '';
+  document.getElementById('codiceCP').innerHTML = '';
+  document.getElementById('costobigl').innerHTML = '';
+})
